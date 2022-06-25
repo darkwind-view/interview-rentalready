b0dadcbfd7ac538007929c8d3f24873681ab4aeb
darkwind-view <darkwind8duck@gmail.com>
2022-06-25 12:39:46 +0200

    feat: test factories

---
+++ src/rental/test_factories.py
@@ -0,0 +1,15 @@
+from . import models as db
+import factory
+
+class RentalTestFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = db.Rental
+    name = factory.Faker('name')
+
+
+class ReservationTestFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = db.Reservation
+    rental = factory.SubFactory(RentalTestFactory)
+    checkin = factory.Faker('date')
+    checkout = factory.Faker('date')
--- src/rental/tests.py
+++ src/rental/tests.py
@@ -1,20 +1,25 @@
 import pytest
 from . import models as db
+from .test_factories import RentalTestFactory, ReservationTestFactory
 
-def test_check():
-    assert True
+@pytest.fixture
+def example_data():
+    rental_1 = RentalTestFactory(name="Rental-1")
+    rental_2 = RentalTestFactory(name="Rental-2")
 
+    ReservationTestFactory(rental=rental_1, checkin="2022-01-01", checkout="2022-01-13")
+    ReservationTestFactory(rental=rental_1, checkin="2022-01-20", checkout="2022-02-10")
+    ReservationTestFactory(rental=rental_1, checkin="2022-02-20", checkout="2022-03-10")
+
+    ReservationTestFactory(rental=rental_2, checkin="2022-01-02", checkout="2022-01-20")
+    ReservationTestFactory(rental=rental_2, checkin="2022-01-20", checkout="2022-02-11")
 
 @pytest.mark.django_db
-def test_to_view_reservations():
-    rental_1 = db.Rental(name="Rental-1")
-    rental_2 = db.Rental(name="Rental-2")
-    rental_1.save()
-    rental_2.save()
+def test_to_fill_db(example_data):
 
-    db.Reservation(rental=rental_1, checkin="2022-01-01", checkout="2022-01-13").save()
-    db.Reservation(rental=rental_1, checkin="2022-01-20", checkout="2022-02-10").save()
-    db.Reservation(rental=rental_1, checkin="2022-02-20", checkout="2022-03-10").save()
+    assert db.Rental.objects.count() == 2
+    assert db.Reservation.objects.count() == 5
 
-    db.Reservation(rental=rental_2, checkin="2022-01-02", checkout="2022-01-20").save()
-    db.Reservation(rental=rental_2, checkin="2022-01-20", checkout="2022-02-11").save()
+
+    
+
