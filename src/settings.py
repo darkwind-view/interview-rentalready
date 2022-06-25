553062c516b4b3f1aa07bc04e929923ec4d1f17f
darkwind-view <darkwind8duck@gmail.com>
2022-06-25 20:39:19 +0200

    fix: no secret, no warnings

--- src/settings.py
+++ src/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ib9h6=wu8!z@+svocov-9pwlswb-tz9q9uwfg(2u2k5c39*eo="
+SECRET_KEY = secrets.token_hex(16)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
